05e1b3d4c880769328318174d6cab5dcd5920d47
JurajHanzek <hanzekj@hotmail.com>
2023-03-09 19:54:30 +0100

    init commit23

--- z1.js
+++ z1.js
@@ -15,22 +15,4 @@ MongoClient.connect('mongodb://localhost:27017').then(function(client) {
                 });
         }
     } catch (e){
-        console.log(e);
-    }
-
-      let continuous  = ["No", "year", "month", "day", "hour", "PM2", "PM10", "SO2", "NO2",
-    "CO", "O3", "TEMP", "PRES", "DEWP", "RAIN", "WSPM"]
-    try{
-        for (let variable of continuous){
-            db.collection('data').updateMany(
-                {[variable]: NaN},
-                {$set: {[variable]: -1}},
-                function(err, res){
-                    if (err) throw err;
-                });
-
-        }
-    } catch (e){
-        console.log(e);
-    }
-});
+        S
